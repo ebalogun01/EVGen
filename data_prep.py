4288c1d74c913e3b1ae782278c70a84063e968cc
Emmanuel Balogun <42688418+ebalogun01@users.noreply.github.com>
2025-03-31 21:42:06 -0700

    Update data_prep.py

--- data_prep.py
+++ data_prep.py
@@ -3,8 +3,8 @@ import numpy as np
 import json
 from matplotlib import pyplot as plt
 
-df_raw = pd.read_csv("Data/raw_google_data.csv")
-df_clean = pd.read_csv("Data/clean_google_data.csv")
+df_raw = pd.read_csv("")
+df_clean = pd.read_csv("")
 
 data = pd.read_csv("Data/chargeDataCleanedWithSessionID.csv")
 with open("Data/chargeDriverZipData.json", "r") as read_file:
